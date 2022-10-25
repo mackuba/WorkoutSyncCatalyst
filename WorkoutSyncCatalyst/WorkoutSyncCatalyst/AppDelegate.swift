df133410ff28fc9c2be9f5d847ab19d1145d6248
Kuba Suder <jakub.suder@gmail.com>
2022-10-25 22:31:59 +0200

    sample code loading workouts from HealthKit (iOS only)

--- WorkoutSyncCatalyst/WorkoutSyncCatalyst/AppDelegate.swift
+++ WorkoutSyncCatalyst/WorkoutSyncCatalyst/AppDelegate.swift
@@ -5,15 +5,16 @@
 //  Created by Kuba Suder on 25/10/2022.
 //
 
+import HealthKit
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        authorizeHealthKit()
+
         return true
     }
 
@@ -31,6 +32,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func authorizeHealthKit() {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            print("Sorry, HealthKit is not available")
+            return
+        }
 
+        let dataTypes: Set<HKObjectType> = [HKObjectType.workoutType()]
+        let healthStore = HKHealthStore()
+
+        healthStore.requestAuthorization(toShare: nil, read: dataTypes) { success, error in
+            if success {
+                print("HealthKit authorized")
+
+                let query = HKSampleQuery(
+                    sampleType: .workoutType(),
+                    predicate: nil,
+                    limit: 0,
+                    sortDescriptors: [NSSortDescriptor(
+                        key: HKSampleSortIdentifierEndDate,
+                        ascending: false
+                    )]
+                ) { query, samples, error in
+                    if let samples = samples as? [HKWorkout] {
+                        print("Got \(samples.count) workouts:")
+
+                        for workout in samples.prefix(upTo: 25) {
+                            let type: String
+                            switch workout.workoutActivityType {
+                            case .running:
+                                type = "running 🏃🏻‍♂️"
+                            case .walking:
+                                type = "walking 🚶🏻‍♂️"
+                            case .hiking:
+                                type = "hiking 🏔"
+                            case .cycling:
+                                type = "cycling 🚴🏻‍♂️"
+                            default:
+                                type = "other (\(workout.workoutActivityType.rawValue))"
+                            }
+
+                            let formatter = DateComponentsFormatter()
+                            formatter.allowedUnits = [.hour, .minute]
+                            formatter.unitsStyle = .abbreviated
+
+                            print("At \(workout.startDate): \(type), time = \(formatter.string(from: workout.duration)!), distance = \(workout.totalDistance!)")
+                        }
+                    } else {
+                        print("Error loading workouts: \(error)")
+                    }
+                }
+
+                healthStore.execute(query)
+            } else {
+                print("Error: \(error)")
+            }
+        }
+    }
 }
 
